8c84e08db7bf4bca111cc46147af3bbed2a73960
sabaruddinlaode <sabaruddinlaode@gmail.com>
2026-07-22 09:55:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,16 @@
    Main Application
 ========================================== */
 
-console.log("HR Interview Management");
+/* ==========================================
+   GLOBAL VARIABLE
+========================================== */
+
+let DATA = [];
+let IMPORT_DATA = [];
+let TEMPLATE = "";
+
+let BROADCAST = [];
+let INDEX_BROADCAST = 0;
+let DELAY_BROADCAST = 2000;
+
+let noWaKonfirmasi = "";
